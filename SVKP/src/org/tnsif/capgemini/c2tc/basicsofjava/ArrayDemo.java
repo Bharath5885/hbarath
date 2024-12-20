11bf7718b5f50aac80b19116184a2cb38614d606
nicky <nicky@192.168.1.44>
2024-12-20 15:26:15 +0530

    1 st commit

---
+++ SVKP/src/org/tnsif/capgemini/c2tc/basicsofjava/ArrayDemo.java
@@ -0,0 +1,12 @@
+package org.tnsif.capgemini.c2tc.basicsofjava;
+
+
+public class ArrayDemo {
+
+	public static void main(String[] args) {
+		int num[] = {4,5,7};
+		System.out.println(num[0]);
+		System.out.println(num[2]);
+	}
+
+}
